18ea1b1bc2e08e279afe35f17927d3292bd525c7
Julien <julien7henry@gmail.com>
2022-09-29 17:31:54 +0200

    adding of scroll to top button

---
+++ components/Navigation/ScrollToTopButton.tsx
@@ -0,0 +1,29 @@
+import { FC, useCallback } from "react";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Fade from "@mui/material/Fade";
+
+const ScrollToTopButton: FC = (): JSX.Element => {
+  const trigger = useScrollTrigger();
+
+  const scrollToTop = useCallback(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        role="presentation"
+        sx={{ position: "fixed", bottom: 75, right: 16, zIndex: 1 }}
+      >
+        <Fab onClick={scrollToTop} size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Box>
+    </Fade>
+  );
+};
+
+export default ScrollToTopButton;
